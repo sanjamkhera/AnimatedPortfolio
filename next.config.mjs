cd09f1173c65b4bfc5a99ec60a728b40ddf50b68
sanjamkhera <88257752+sanjamkhera@users.noreply.github.com>
2024-03-04 00:51:56 -0800

    Initial Commit

---
+++ next.config.mjs
@@ -0,0 +1,14 @@
+// next.config.mjs
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns:[
+      {
+        protocol:"https",
+        hostname:"images.pexels.com",
+      }
+    ]
+  },
+};
+
+export default nextConfig;
