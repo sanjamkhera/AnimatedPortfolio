e9f01bb1322b36470de6e8ee448a8d8ca402508a
sanjamkhera <88257752+sanjamkhera@users.noreply.github.com>
2024-04-28 21:01:13 -0700

    Functioning Portfolio

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -43,7 +43,7 @@ const Homepage = () => {
             Deployment is handled using AWS Elastic Beanstalk, with Docker providing containerization.
           </p>
           <p className="text-sm mt-2">
-            The website's content includes four pages About Me, Contact Information, Portfolio Showcase, Home, Brain (dedicated to SVG),
+            The websites content includes four pages About Me, Contact Information, Portfolio Showcase, Home, Brain (dedicated to SVG),
             Navigation Bar, Navigation Links, and Transition Provider.
             DevOps files like Dockerfile and docker-compose.yml are included for creating a Docker image and defining services, networks, and volumes.
           </p>
@@ -62,14 +62,14 @@ const Homepage = () => {
           <p className="text-sm mt-2">In his second year, John delved deeper into the world of Computer Science. He studied advanced topics like operating systems, computer networks, and database systems. He also started working on his own projects, applying the theoretical knowledge he gained in practical scenarios.</p>
           <p className="text-sm mt-2">By his third year, John was well-versed in several programming languages and had a solid understanding of software development principles. He took elective courses in Artificial Intelligence and Machine Learning, which sparked his interest in data science.</p>
           <p className="text-sm mt-2">In his final year, John worked on a capstone project that involved developing a machine learning model to predict stock market trends. He also completed an internship at a local tech company, gaining valuable industry experience.</p>
-          <p className="text-sm mt-2">Throughout his four years at the University of Manitoba, John not only acquired a strong foundation in Computer Science but also developed critical thinking and problem-solving skills that prepared him for the tech industry."</p>
+          <p className="text-sm mt-2">Throughout his four years at the University of Manitoba, John not only acquired a strong foundation in Computer Science but also developed critical thinking and problem-solving skills that prepared him for the tech industry.</p>
         </div>
         <div className="container m-auto p-4 text-justify bg-white rounded-lg mt-12">
           <h3 className="text-xl font-bold">Mechanical Engineering Exp</h3>
           <p className="text-sm mt-2">My journey in Mechanical Engineering began at Red River College, where I pursued a Mechanical Engineering Technology program from August 2012 to December 2015.</p>
           <p className="text-sm mt-2">My first professional experience was as a Controls Technician at Mikkelsen Coward, where I created wiring layouts and installation packages for electricians and developed sequences of operations for programmers.</p>
           <p className="text-sm mt-2">I then moved on to VAW Systems as an Equipment Designer. Here, I worked with Project Managers and customers to interpret specifications, used 3D modeling tools like Inventor for design documentation, and created manufacturing packages using AutoCAD. I also took the initiative to automate repetitive tasks and worked on standardizing and improving existing product designs.</p>
-          <p className="text-sm mt-2">Most recently, I worked as a Mechanical Designer at K-HART Industries. I was part of the team that developed hydraulic systems for farm equipment and created macros for 3D modeling in On-Shape. I also managed version control using On-Shape's in-built release management and used tools like Sim-Solid for stress analysis and Odoo for operational management.</p>
+          <p className="text-sm mt-2">Most recently, I worked as a Mechanical Designer at K-HART Industries. I was part of the team that developed hydraulic systems for farm equipment and created macros for 3D modeling in On-Shape. I also managed version control using On-Shapes in-built release management and used tools like Sim-Solid for stress analysis and Odoo for operational management.</p>
           <p className="text-sm mt-2">Throughout my career, I have worked on numerous projects, including the design of a hydraulic lift system for a farm seeding drill and the creation of drawing packages for over 1200 custom industrial silencers. I have also reverse-engineered a hydraulic bending machine and designed an industrial-sized bucket elevator for a farming application.</p>
         </div>
         <div className="container m-auto p-4 text-justify bg-white rounded-lg mt-12">
